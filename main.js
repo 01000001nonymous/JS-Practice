3d1db6a9bd343c66156577448c87ae9bac1c9df0
0100000nonymous <meemcpp@gmail.com>
2026-01-23 01:35:48 +0200

    check if url contain hash

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-const img = document.getElementById('img'),
-  imgs = [
-    'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kaW5nfGVufDB8fDB8fHww',
-    'https://images.unsplash.com/photo-1542831371-29b0f74f9713?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29kaW5nfGVufDB8fDB8fHww',
-    'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kaW5nfGVufDB8fDB8fHww',
-    'https://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y29kaW5nfGVufDB8fDB8fHww',
-  ],
-  randomNumber = () => Math.floor(Math.random() * imgs.length);
-
-  const randomImg = (img, arr) => {
-    setInterval(() => {
-      img.src = arr[randomNumber()];
-  }, 3000);
+if (window.location.hash) {
+  if (window.location.hash.indexOf("anything") === 1) {
+    console.log("Hash contains anything");
+  } else {
+    console.log("The hash exists but doesn't contain anything");
+  }
+} else {
+  console.log("Hash doesn't contain anything");
 }
-randomImg(img, imgs);
