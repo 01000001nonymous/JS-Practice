579da328cb5135419f39901cab581ba537dcbe4b
0100000nonymous <meemcpp@gmail.com>
2026-01-23 00:43:37 +0200

    show random images

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-addZero = (num) => (num < 10 ? "0" + num : num);
-showTime = () => {
-  const now = new Date(),
-    hours = now.getHours(),
-    minutes = now.getMinutes(),
-    seconds = now.getSeconds();
-  const formattedTime = `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
-  document.getElementById("clock").innerText = formattedTime;
-};
+const img = document.getElementById('img'),
+  imgs = [
+    'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kaW5nfGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1542831371-29b0f74f9713?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29kaW5nfGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29kaW5nfGVufDB8fDB8fHww',
+    'https://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y29kaW5nfGVufDB8fDB8fHww',
+  ],
+  randomNumber = () => Math.floor(Math.random() * imgs.length);
 
-setInterval(showTime, 1000);
-showTime();
+  const randomImg = (img, arr) => {
+    setInterval(() => {
+      img.src = arr[randomNumber()];
+  }, 3000);
+}
+randomImg(img, imgs);
