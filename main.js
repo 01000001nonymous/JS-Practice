34a2a8a8d1889a3e75b9216f2ec9b1a834a689ec
0100000nonymous <meemcpp@gmail.com>
2026-01-25 05:07:09 +0200

    to do list

--- main.js
+++ main.js
@@ -1,15 +1,82 @@
-let startPoint = 15;
-const count = document.getElementById("count"),
-  addZero = (num) => (num < 10 ? "0" + num : num),
-  countDown = setInterval(() => {
-    let minutes = Math.floor(startPoint / 60),
-      seconds = startPoint % 60;
-    count.textContent = `${addZero(minutes)}:${addZero(seconds)}`;
+const input = document.querySelector(".input"),
+  add = document.querySelector(".add"),
+  divTasks = document.querySelector(".tasks");
 
-    if (startPoint > 0) {
-      startPoint--;
-    } else {
-      clearInterval(countDown);
-      count.textContent = "Time's up!";
+let tasks = [];
+
+if(localStorage.getItem('tasks')) tasks = JSON.parse(localStorage.getItem('tasks'));
+
+getLS();
+
+divTasks.addEventListener('click', (e) => {
+  if(e.target.classList.contains('delete')) {
+    removeTask(e.target.parentElement.getAttribute('id'));
+    e.target.parentElement.remove();
+  }
+  if(e.target.classList.contains('task')) {
+    toggleStatus(e.target.getAttribute('id'));
+    e.target.classList.toggle('done');
+  }
+})
+
+add.onclick = (e) => {
+e.preventDefault();
+  if (input.value !== "") {
+    addTask(input.value);
+    input.value = "";
+  }
+};
+
+function addTask(inputTxt) {
+  const task = {
+    id: crypto.randomUUID(),
+    text: inputTxt,
+    completed: false,
+  };
+  tasks.push(task);
+  makeEle(tasks);
+  setLS(tasks);
+};
+
+function makeEle(tasks) {
+  divTasks.innerHTML = "";
+  tasks.forEach((task) => {
+    const div = document.createElement("div");
+    div.className = "task";
+    if (task.completed) div.className = "task done";
+    div.setAttribute("id", task.id);
+    div.appendChild(document.createTextNode(task.text));
+    const span = document.createElement("span");
+    span.className = 'delete';
+    span.appendChild(document.createTextNode("Delete"));
+    div.appendChild(span);
+    divTasks.appendChild(div);
+  });
+};
+
+function setLS() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+function getLS() {
+  const data = localStorage.getItem("tasks");
+  if (data) {
+    const tasks = JSON.parse(data);
+    makeEle(tasks);
+  }
+};
+
+function removeTask(id) {
+  tasks = tasks.filter((task) => task.id != id );
+  setLS(tasks);
+}
+
+function toggleStatus(id) {
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id == id) {
+      tasks[i].completed = !tasks[i].completed;
+      break;
     }
-  }, 1000);
+  }
+  setLS(tasks);
+}
